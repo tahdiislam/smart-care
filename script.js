26b1c828def51c5932e1d3df559d025bc7e852df
Tahdi Islam <tshdi60@gmail.com>
2024-06-09 11:37:21 +0600

    complete review section

--- script.js
+++ script.js
@@ -1,30 +1,145 @@
-const services = document.getElementById('services');
+/** @format */
+
+const services = document.getElementById("services");
+const designations = document.getElementById("designation");
+const specializations = document.getElementById("specialization");
+const doctors = document.getElementById("doctors");
+const nodata = document.getElementById("nodata");
+const spinner = document.getElementById("spinner");
+const reviews = document.getElementById("reviews");
 
 const loadServices = () => {
-    fetch('https://testing-8az5.onrender.com/services/')
-        .then(res => res.json())
-        .then(data => displayServices(data))
-        .catch(error => console.log(error))
-}
+  fetch("https://testing-8az5.onrender.com/services/")
+    .then((res) => res.json())
+    .then((data) => displayServices(data))
+    .catch((error) => console.log(error));
+};
 
 const displayServices = (data) => {
-    data?.map(service => {
-        const serviceList = document.createElement('li');
-        serviceList.innerHTML = `
+  data?.map((service) => {
+    const serviceList = document.createElement("li");
+    serviceList.innerHTML = `
             <div class="card shadow h-100">
                 <div class="ratio ratio-16x9">
-                    <img src="${service.image}" class="card-img-top" loading="lazy" alt="${service.name}">
+                    <img src="${
+                      service.image
+                    }" class="card-img-top" loading="lazy" alt="${
+      service.name
+    }">
                 </div>
                 <div class="card-body p-3 p-xl-5">
                     <h3 class="card-title h5">${service.name}</h3>
-                    <p class="card-text" style='width:100%'>${service.description.slice(0, 100)}</p>
+                    <p class="card-text" style='width:100%'>${service.description.slice(
+                      0,
+                      100
+                    )}</p>
                     <div><a href="#" class="btn btn-primary">See Details</a>
                     </div>
                 </div>
             </div>
-        `
-        services.appendChild(serviceList);
-    })
-}
+        `;
+    services.appendChild(serviceList);
+  });
+};
 
-loadServices()
+const loadDesignation = () => {
+  fetch("https://test-thto.onrender.com/doctor/designation/")
+    .then((res) => res.json())
+    .then((data) => {
+      data?.map((designation) => {
+        const designationList = document.createElement("li");
+        designationList.innerHTML = `
+                    <button onclick="loadDoctors('${designation.slug}')" class="dropdown-item" href="#">${designation.name}</button>
+                `;
+        designations.appendChild(designationList);
+      });
+    })
+    .catch((error) => console.log(error));
+};
+
+const loadSpecialization = () => {
+  fetch("https://test-thto.onrender.com/doctor/specialization/")
+    .then((res) => res.json())
+    .then((data) => {
+      data?.map((specialization) => {
+        const specializationList = document.createElement("li");
+        specializationList.innerHTML = `
+                    <button onclick="loadDoctors('${specialization.slug}')" class="dropdown-item" href="#">${specialization.name}</button>
+                `;
+        specializations.appendChild(specializationList);
+      });
+    })
+    .catch((error) => console.log(error));
+};
+
+const loadDoctors = (search) => {
+  doctors.innerText = "";
+  spinner.style.display = "block";
+  nodata.style.display = "none";
+  fetch(
+    `https://test-thto.onrender.com/doctor/list/?search=${search ? search : ""}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      if (data?.count === 0) {
+        console.log("hello wo3rld");
+        spinner.style.display = "none";
+        nodata.style.display = "block";
+      } else {
+        spinner.style.display = "none";
+        nodata.style.display = "none";
+        data?.results?.map((doctor) => {
+          const div = document.createElement("div");
+          div.classList.add("col-4");
+          div.innerHTML = `
+                <div class="card w-100">
+                    <img src="${doctor.image}" class="doc_card_img rounded-circle m-auto" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${doctor.full_name}</h5>
+                        <p class="card-text">${doctor.designation}</p>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, exercitationem reiciendis accusamus</p>
+                            <p ><small class="text-muted">${doctor.specialization}</small></p>
+                        <a href="#" class="btn btn-primary">View Details</a>
+                    </div>
+                </div>
+                `;
+          doctors.appendChild(div);
+        });
+      }
+    });
+};
+
+const handleSearch = (event) => {
+  event.preventDefault();
+  const search = document.getElementById("search").value;
+  loadDoctors(search);
+};
+
+const loadReviews = () => {
+  fetch("https://test-thto.onrender.com/doctor/review/")
+    .then((res) => res.json())
+    .then((data) => {
+      data?.map((review) => {
+        const li = document.createElement("li");
+        li.innerHTML = `<div class="review_card shadow-lg">
+                        <div class="d-flex justify-content-between gap-3">
+                            <img src="./Images/girl.png" alt="">
+                            <div>
+                                <h3>${review.reviewer}</h3>
+                                <p>${review.rating}</p>
+                            </div>
+                        </div>
+                        <p>
+                           ${review.body.slice(0, 150)} 
+                        </p>
+                    </div>`;
+        reviews.appendChild(li);
+      });
+    });
+};
+
+loadServices();
+loadDesignation();
+loadSpecialization();
+loadDoctors();
+loadReviews()
