a685c55ea52d022c34a48bee635e170af192aa97
Tahdi Islam <tshdi60@gmail.com>
2024-06-10 14:27:14 +0600

    add login and registration page

--- doctor.js
+++ doctor.js
@@ -1,10 +1,13 @@
 /** @format */
 
+const param = new URLSearchParams(window.location.search).get("docId");
+
 const docDetails = document.getElementById("docDetails");
 const reviews = document.getElementById("doctor-reviews");
+const times = document.getElementById("time");
+const reviewContainer = document.getElementById('doctor-reviews-container')
 
 const getParams = () => {
-  const param = new URLSearchParams(window.location.search).get("docId");
   fetch(`https://test-thto.onrender.com/doctor/list/${param}`)
     .then((res) => res.json())
     .then((data) => displayDetails(data));
@@ -20,7 +23,7 @@ const displayDetails = (data) => {
                 <div class="d-flex justify-content-center align-items-start gap-5 details">
                 <img src="${data?.image}" alt="" srcset="">
                 <div class="w-50">
-                    <h1>
+                    <h1 class="doctor_name">
                        ${data?.full_name} 
                     </h1>
                     <p>${data?.specialization}</p>
@@ -32,14 +35,18 @@ const displayDetails = (data) => {
                         minus maiores, sit neque vitae commodi consectetur?
                     </p>
                     <h3>Fees: ${data?.fee} BDT</h3>
-                    <button class="btn btn-primary">Take Appointment</button>
-                    <div>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Take Appointment
+                    </button>
                 </div>
             </div>`;
   docDetails.appendChild(div);
 };
 
 const displayReviews = (data) => {
+  if(data.length === 0){
+    reviewContainer.style.display = 'none'
+  }
   data?.map((review) => {
     const li = document.createElement("li");
     li.innerHTML = `<div class="review_card shadow-lg">
@@ -58,4 +65,46 @@ const displayReviews = (data) => {
   });
 };
 
+const loadAvailableTimes = () => {
+  fetch(
+    `https://test-thto.onrender.com/doctor/availabletime/?doctor_id=${param}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      data?.map((time) => {
+        const option = document.createElement("option");
+        option.value = time.id;
+        option.innerHTML = time.name;
+        times.appendChild(option);
+      });
+    });
+};
+
+const submitForm = () => {
+  const status = document.getElementsByName("status");
+  const selectedStatus = Array.from(status).find((button) => button.checked);
+  const symptom = document.getElementById("symptom").value;
+  const time = document.getElementById("time").value;
+  const info = {
+    appointment_type: selectedStatus.value,
+    appointment_status: "Pending",
+    symptom: symptom,
+    cancel: false,
+    patient: 1,
+    doctor: param,
+    time: time,
+  };
+  console.log(info);
+  fetch("https://test-thto.onrender.com/appointment/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(info),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+};
+
 getParams();
+loadAvailableTimes();
